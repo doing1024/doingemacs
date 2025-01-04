13682ea280060de5e4fa3517f0680624e0697704
doing1024 <doing1024@outlook.it>
2025-01-04 11:40:19 +0800

    fix:使用submodules来管理插件

--- site-lisp/swiper/targets/checkdoc.el
+++
@@ -1,7 +0,0 @@
-;; Copyright (C) 2017-2024 Free Software Foundation, Inc.
-(checkdoc-file "colir.el")
-(checkdoc-file "counsel.el")
-(checkdoc-file "ivy-overlay.el")
-(checkdoc-file "ivy-test.el")
-(checkdoc-file "ivy.el")
-(checkdoc-file "swiper.el")
